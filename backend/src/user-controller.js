71f1f2de2625112913a2e7d3a642a33d2b83cdff
Mats Jun Larsen <matsjla@stud.ntnu.no>
2023-02-09 08:44:18 +0100

    Implement API endpoint for logging in users

--- backend/src/user-controller.js
+++ backend/src/user-controller.js
@@ -1,10 +1,21 @@
-import { createUserSchema } from "./user.js";
+import { createUserSchema, loginUserSchema } from "./user.js";
 
 export class UserController {
   #userService;
 
   constructor(userService) {
     this.#userService = userService;
+  }
+
+  async login(req, res) {
+    const form = loginUserSchema.parse(req.body);
+    const login = await this.#userService.login(form.email, form.password);
+    if (typeof login === "object") {
+      return res.status(401, login).json(login);
+    }
+    return res.status(200).json({
+      jwt: login,
+    });
   }
 
   async register(req, res) {
